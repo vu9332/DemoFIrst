2ec742e092124cb3759c0d954f10843dc62d1ac6
vu <nguyenmanh9332@gmail.com>
2024-05-09 19:45:36 +0700

    first

---
+++ bai2/Program.cs
@@ -0,0 +1,52 @@
+﻿namespace bai2
+{
+  
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DemoHashSet();
+            ListDemo();
+        }
+
+        static void DemoHashSet()
+        {
+            var dayOfWeeks=new HashSet<string>();
+            dayOfWeeks.Add("Monday");
+            dayOfWeeks.Add("Tues");
+            dayOfWeeks.Add("Wed");
+            dayOfWeeks.Add("Tues");
+            foreach(var item in dayOfWeeks)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("SophanTu: "+ dayOfWeeks.Count);
+        }
+        static void ListDemo()
+        {
+            var listTxt = new List<string>();
+            listTxt.Add("lmht");
+            listTxt.Add("genshin");
+            listTxt.Add("GTA 5");
+            listTxt.Add("CSGO");
+            int i = 0;
+
+
+            foreach (var item in listTxt)
+            {
+                Console.WriteLine("truoc: ");
+
+               Console.WriteLine( item);
+            }
+            listTxt.Insert(i, "valorant");
+
+            foreach (var item in listTxt)
+            {
+                Console.WriteLine("sau: ");
+                Console.WriteLine(item);
+            }
+
+        }
+
+    }
+}
